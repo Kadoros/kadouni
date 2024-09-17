8866849df471c5a1f6cca433b8eab2da723e9e75
kado1130 <kadoros1130@gmail.com>
2024-09-17 23:34:13 +0200

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { getSession, SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,14 +20,16 @@ export default async function RootLayout({
 }>) {
   return (
     <SessionProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <Toaster />
-          <div className="h-full flex items-center justify-center bg-slate-300">
-            {children}
-          </div>
-        </body>
-      </html>
+      <Suspense fallback={<div>Loading...</div>}>
+        <html lang="en">
+          <body className={inter.className}>
+            <Toaster />
+            <div className="h-full flex items-center justify-center bg-slate-300">
+              {children}
+            </div>
+          </body>
+        </html>
+      </Suspense>
     </SessionProvider>
   );
 }
